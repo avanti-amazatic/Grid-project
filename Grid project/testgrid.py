43c10ca547570df64d207472c68c162d33c1bca9
avanti <avanti.d@amazatic.com>
2015-07-16 13:08:38 +0530

    Commit github

--- Grid project/testgrid.py
+++ Grid project/testgrid.py
@@ -1,6 +1,6 @@
 import sys
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+#from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
 import unittest
 from selenium.webdriver.support import expected_conditions as EC
